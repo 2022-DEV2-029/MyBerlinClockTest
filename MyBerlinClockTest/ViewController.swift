ccecd51a47f8e0ce334fd6c10c10876c2c7e720b
2022-DEV2-029 <2022dev2029@gmail.com>
2022-12-26 12:05:06 +0100

    Init xcode project

---
+++ MyBerlinClockTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyBerlinClockTest
+//
+//  Created by 2022-DEV2-029 on 26/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
+
---
+++ MyBerlinClockTestTests/MyBerlinClockTestTests.swift
@@ -0,0 +1,13 @@
+//
+//  MyBerlinClockTestTests.swift
+//  MyBerlinClockTestTests
+//
+//  Created by 2022-DEV2-029 on 26/12/2022.
+//
+
+import XCTest
+@testable import MyBerlinClockTest
+
+final class MyBerlinClockTestTests: XCTestCase {
+
+}
